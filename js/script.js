5889f89ce90fcf05065a6108291428b564c980c6
Chutimon Srisombun <chutimonsrisombun@192.168.0.110>
2023-12-05 13:16:53 +0700

    pop up button when clicking search button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 let searchBtn = document.querySelector('#search-btn');
 let searchBar = document.querySelector('.search-bar-container');
 
-searchBtn .addEventListener('cick', () =>{
+searchBtn.addEventListener('click', () => {
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
-
 });
