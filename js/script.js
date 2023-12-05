c3df0a99026092080d840649d88e6a38acf6c6c5
Chutimon Srisombun <chutimonsrisombun@192.168.0.110>
2023-12-05 21:29:31 +0700

    created login and register form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 let searchBtn = document.querySelector('#search-btn');
 let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#login-close');
+
+window.onscroll = () =>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+
+}
 
 searchBtn.addEventListener('click', () => {
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () => {
+    loginForm.classList.toggle('active');
+});
+
+formClose.addEventListener('click', () => {
+    loginForm.classList.remove('active');
 });
